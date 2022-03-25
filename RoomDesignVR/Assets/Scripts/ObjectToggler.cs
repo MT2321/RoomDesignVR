f28b6274128522be279e5fffbfbaa60778d1ef7e
Martin.Turco <mrodriguez@itba.edu.ar>
2022-03-24 23:00:34 -0300

    Developed Object Toggler

---
+++ RoomDesignVR/Assets/Scripts/ObjectToggler.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectToggler : MonoBehaviour
+{
+    public GameObject character;
+
+    private int minInteractionDistance = 2;
+    private GameObject[] gos;
+    private int currentVisibleObject = 0;
+    void Start()
+    {
+        print("Object Toggler Running");
+        gos = GameObject.FindGameObjectsWithTag(tag);
+        Debug.Log("Cantidad con tag: " + tag + " " + gos.Length);
+
+        for (int i = 0; i < gos.Length; i++)
+        {
+            if (gos[i].name != this.transform.name)
+                SetVisibility(gos[i], false);
+        }
+    }
+
+    void Update()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(character.transform.position, character.transform.TransformDirection(Vector3.forward), out hit, minInteractionDistance))
+        {
+            Debug.Log("Object Toggler: " + hit.transform.name);
+            if (Input.GetKeyDown(KeyCode.G) && hit.transform.tag == tag)
+                ShowNextObject(hit.transform.tag, hit.transform.name);
+        }
+    }
+
+    private void ShowNextObject(string tag, string name)
+    {
+        Debug.Log("Toggling " + currentVisibleObject);
+        GameObject g_old_object = gos[currentVisibleObject];
+        SetVisibility(g_old_object, false);
+        currentVisibleObject = Math.Max(0, (currentVisibleObject + 1) % gos.Length);
+        GameObject g_new_object = gos[currentVisibleObject];
+        SetVisibility(g_new_object, true);
+
+    }
+
+    private void SetVisibility(GameObject g, Boolean state)
+    {
+        if (g.GetComponent<Collider>())
+            g.GetComponent<Collider>().enabled = state;
+        if (g.GetComponent<CapsuleCollider>())
+            g.GetComponent<CapsuleCollider>().enabled = state;
+
+        g.GetComponent<Renderer>().enabled = state;
+
+    }
+
+
+}
